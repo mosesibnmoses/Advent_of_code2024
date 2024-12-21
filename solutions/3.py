91b1af57c371637e9cfd1051d91c6a3b7f5af5f6
dmoses <mosesibnmoses@gmail.com>
2024-12-20 19:19:43 -0600

    refactor

--- solutions/3.py
+++ solutions/3.py
@@ -4,7 +4,7 @@ from utils.helpers import parse_input
 
 product=0
 pattern=r"mul\(([0-9]{1,3}),([0-9]{1,3})\)"
-lines = parse_input('../data/3_input.txt')
+lines = parse_input('../data/3_input.txt',True)
 print (lines)
 commands=re.findall(pattern,lines)
 print (commands)
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,6 @@
-def parse_input(file_path):
+def parse_input(file_path,whole=False):
     """Read a file and return a list of stripped lines."""
     with open(file_path, 'r') as f:
+        if whole:
+            return f.read()
         return [line.strip() for line in f]
